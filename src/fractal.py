fdda50722bb806b41884095d641047ca82bb3492
Jaime96 <jaimesko96@gmail.com>
2023-03-26 18:38:16 -0400

    Added main.py and fractal.py

---
+++ src/fractal.py
@@ -0,0 +1,26 @@
+import numpy as np
+import itertools
+
+def mandelbrot(xo: float|int = 2.47/2, yo: float|int = 0, res: int = 200, maxiter: int = 1000, zoom: int|float = 1) -> np.ndarray:
+    xrange = 2.47 / zoom
+    yrange = 2.24 / zoom
+    xmin = xo - xrange/2
+    xmax = xo + xrange/2
+    ymin = yo - yrange/2
+    ymax = yo + yrange/2
+    
+    rx = int(2.47 * res)
+    ry = int(2.24 * res)
+    
+    X = np.linspace(xmin, xmax, rx)
+    Y = np.linspace(ymin, ymax, ry)
+    image = np.empty((len(Y), len(X)))
+    for (j,y), (i,x) in itertools.product(enumerate(Y), enumerate(X)):
+        c = complex(x, y)
+        z = 0
+        iter = 0
+        while iter < maxiter and (z*z.conjugate()).real < 4:
+            z = z**2 + c
+            iter += 1
+        image[-(j-1),i] = iter
+    return image
---
+++ src/main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from fractal import mandelbrot
+
+xmin, xmax, ymin, ymax = -2., 0.47, -1.12, 1.12
+xo, yo = (xmin + xmax)/2, (ymin + ymax)/2
+r = 200
+maxiter = 100
+z = 1
+
+if __name__ == "__main__":
+    st.title("Mandelbrot Fractal")
+
+    st.sidebar.title("Settings")
+    x0 = st.sidebar.slider("X Center", xmin, xmax, xo)
+    y0 = st.sidebar.slider("Y Center", ymin, ymax, yo)
+    z = st.sidebar.slider("Zoom", 0.5, 10., 1., 0.1, "%.1f")
+    r = st.sidebar.slider("Resolution", 100, 500, r) # r = st.sidebar.slider("Resolution", 0.001, 0.01, r, 0.001, "%.3f")
+    maxiter = st.sidebar.slider("Max Iterations", 10, 200, maxiter)
+    cg = st.sidebar.slider("cg", 0.1, 2., 1., 0.1, "%.1f")
+    cmap = st.sidebar.radio("Color map", ("plasma", "twilight_shifted", "seismic"), index=1)
+
+    image = mandelbrot(x0,y0, r, maxiter, z)
+    fig = plt.figure()
+    plt.imshow(image**cg, cmap=cmap) #plasma, twilight_shifted, seismic
+    plt.axis('off')
+    st.pyplot(fig)
